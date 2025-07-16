3b238076aa1f0765432acd0f40b4e6707aee80c6
Andy Stanberry <andystanberry@gmail.com>
2025-07-16 09:27:56 +0100

    Reemove unused export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { default as knexPaginator } from './orm-connectors/knex';
 export {
   default as dynamodbPaginator,
-  PagerEntityAccessPattern,
   cursorGenerator as dynamodbCursorGenerator,
   convertNodesToEdges as dynamodbConvertNodesToEdges,
 } from './orm-connectors/dynamodb-toolbox';
